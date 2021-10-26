d64aa968a1ebb065f75f8c8e256f935f9adfdcbb
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-26 17:12:23 +1000

    feat: Added SaveFileArguments text support.

--- src/libs/H.ReactiveUI.CommonInteractions.Core/Models/SaveFileArguments.cs
+++ src/libs/H.ReactiveUI.CommonInteractions.Core/Models/SaveFileArguments.cs
@@ -1,4 +1,6 @@
-﻿namespace H.ReactiveUI;
+﻿using System.Text;
+
+namespace H.ReactiveUI;
 
 public class SaveFileArguments
 {
@@ -9,6 +11,24 @@ public class SaveFileArguments
     public string FilterName { get; init; } = string.Empty;
     public Func<Task<byte[]>>? BytesFunc { get; init; }
     public byte[] Bytes { get; init; } = Array.Empty<byte>();
+
+    /// <summary>
+    /// Uses <see cref="Encoding.UTF8"/> to convert.
+    /// </summary>
+    public Func<Task<string>> TextFunc
+    {
+        get => () => Task.FromResult(string.Empty);
+        init => BytesFunc = async () => Encoding.UTF8.GetBytes(await value().ConfigureAwait(false));
+    }
+
+    /// <summary>
+    /// Uses <see cref="Encoding.UTF8"/> to convert.
+    /// </summary>
+    public string Text
+    {
+        get => Encoding.UTF8.GetString(Bytes);
+        init => Bytes = Encoding.UTF8.GetBytes(value);
+    }
 
     #endregion
 }
