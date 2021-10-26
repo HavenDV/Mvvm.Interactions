4c836b00ebec96e84b96f7e252eaa52e3f01ec6b
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-26 15:24:37 +1000

    refactor: Added WebInteractionManager.

--- src/shared/H.ReactiveUI.CommonInteractions.Shared/InteractionManager.cs
+++ src/shared/H.ReactiveUI.CommonInteractions.Shared/InteractionManager.cs
@@ -1,11 +1,4 @@
-﻿using System.Reactive;
-#if WPF
-using System.Diagnostics;
-#else
-using Windows.System;
-#endif
-
-namespace H.ReactiveUI;
+﻿namespace H.ReactiveUI;
 
 public class InteractionManager
 {
@@ -32,33 +25,8 @@ public class InteractionManager
     {
         MessageInteractionManager.Register();
         FileInteractionManager.Register();
-#if WPF
-        _ = WebInteractions.OpenUrl.RegisterHandler(static context =>
-        {
-            var url = context.Input;
-
-            _ = Process.Start(new ProcessStartInfo(url)
-            {
-                UseShellExecute = true,
-            });
-
-            context.SetOutput(Unit.Default);
-        });
-#else
-        _ = WebInteractions.OpenUrl.RegisterHandler(static async context =>
-        {
-            var url = context.Input;
-
-            _ = await Launcher.LaunchUriAsync(new Uri(url))
-#if Uno
-                .ConfigureAwait(false)
-#endif
-                ;
-
-            context.SetOutput(Unit.Default);
-        });
-#endif
-
-        #endregion
+        WebInteractionManager.Register();
     }
+
+    #endregion
 }
---
+++ src/shared/H.ReactiveUI.CommonInteractions.Shared/Managers/WebInteractionManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Reactive;
+#if WPF
+using System.Diagnostics;
+#else
+using Windows.System;
+#endif
+
+namespace H.ReactiveUI;
+
+public static class WebInteractionManager
+{
+    #region Methods
+
+    public static void Register()
+    {
+#if WPF
+        _ = WebInteractions.OpenUrl.RegisterHandler(static context =>
+        {
+            var url = context.Input;
+
+            _ = Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true,
+            });
+
+            context.SetOutput(Unit.Default);
+        });
+#else
+        _ = WebInteractions.OpenUrl.RegisterHandler(static async context =>
+        {
+            var url = context.Input;
+
+            _ = await Launcher.LaunchUriAsync(new Uri(url))
+#if Uno
+                .ConfigureAwait(false)
+#endif
+                ;
+
+            context.SetOutput(Unit.Default);
+        });
+#endif
+    }
+
+    #endregion
+}
