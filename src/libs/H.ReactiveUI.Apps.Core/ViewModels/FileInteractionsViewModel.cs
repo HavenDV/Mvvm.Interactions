f7097a871f11c7a91ef3c714eb08e876f90d34ab
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-11-02 19:58:01 +1000

    example: Implemented FileInteractionsViewModel.

--- src/libs/H.ReactiveUI.Apps.Core/ViewModels/FileInteractionsViewModel.cs
+++ src/libs/H.ReactiveUI.Apps.Core/ViewModels/FileInteractionsViewModel.cs
@@ -2,4 +2,39 @@
 
 public class FileInteractionsViewModel
 {
+    #region Properties
+
+    public ReactiveCommand<Unit, FileData?> OpenFile { get; }
+    public ReactiveCommand<Unit, FileData[]> OpenFiles { get; }
+    public ReactiveCommand<Unit, string?> SaveFile { get; }
+    public ReactiveCommand<Unit, string?> SaveOpenFile { get; }
+
+    public ReactiveCommand<Unit, Unit> LaunchInTemp { get; }
+    public ReactiveCommand<Unit, Unit> LaunchPath { get; }
+    public ReactiveCommand<Unit, Unit> LaunchFolder { get; }
+
+    #endregion
+
+    #region Constructors
+
+    public FileInteractionsViewModel()
+    {
+        OpenFile = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.OpenFile.Handle(new OpenFileArguments()));
+        OpenFiles = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.OpenFiles.Handle(new OpenFileArguments()));
+        SaveFile = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.SaveFile.Handle(new SaveFileArguments()));
+        SaveOpenFile = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.SaveOpenFile.Handle(new SaveOpenFileArguments()));
+
+        LaunchInTemp = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.LaunchInTemp.Handle(new FileData()));
+        LaunchPath = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.LaunchPath.Handle("Are you sure?"));
+        LaunchFolder = ReactiveCommand.CreateFromObservable(
+            () => FileInteractions.LaunchFolder.Handle("Are you sure?"));
+    }
+
+    #endregion
 }
