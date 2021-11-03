d4d820b03e6f797208f6deeb6ce23d0545a8959a
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-11-03 19:33:27 +1000

    feat: Added BooleanToVisibilityConverter.

---
+++ src/shared/H.ReactiveUI.CommonInteractions.Shared/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+
+namespace H.ReactiveUI.Converters;
+
+public class BooleanToVisibilityConverter : BaseConverter<bool, Visibility>
+{
+    #region Static methods
+
+    public static Visibility Convert(bool value)
+    {
+        return value
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+    }
+
+    public static bool ConvertBack(Visibility value)
+    {
+        return value == Visibility.Visible;
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public BooleanToVisibilityConverter() : 
+        base(Convert, ConvertBack)
+    {
+    }
+
+    #endregion
+}
