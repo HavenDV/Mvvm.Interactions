0e69eb29691f2a3b23d12349d6855b8fda1d5dc2
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-11-04 19:37:34 +1000

    example: Refactored WPF project to use shared project.

--- src/shared/H.ReactiveUI.Apps.Shared/GlobalUsings.cs
+++ src/shared/H.ReactiveUI.Apps.Shared/GlobalUsings.cs
@@ -15,14 +15,12 @@ global using System.Windows;
 global using System.Windows.Data;
 global using System.Windows.Media;
 global using System.Windows.Controls;
-global using Icon = MaterialDesignThemes.Wpf.PackIconKind;
 #elif HAS_WINUI
 global using Microsoft.UI;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Data;
 global using Microsoft.UI.Xaml.Media;
 global using Microsoft.UI.Xaml.Controls;
-global using Icon = Microsoft.UI.Xaml.Controls.Symbol;
 global using LaunchActivatedEventArgs = Microsoft.UI.Xaml.LaunchActivatedEventArgs;
 #else
 global using Windows.UI;
@@ -30,5 +28,4 @@ global using Windows.UI.Xaml;
 global using Windows.UI.Xaml.Data;
 global using Windows.UI.Xaml.Media;
 global using Windows.UI.Xaml.Controls;
-global using Icon = Windows.UI.Xaml.Controls.Symbol;
 #endif
