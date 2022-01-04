6f1742cc7dfaa4e1d22d972de4b28358106237a3
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-01-04 23:15:55 +1000

    refactor: Refactored WebInteractionsManager to platform specific files.

---
+++ src/libs/H.ReactiveUI.CommonInteractions.Shared/Managers/Web/WebInteractionManager.Avalonia.cs
@@ -0,0 +1,26 @@
+﻿#if HAS_AVALONIA
+using System.Reactive;
+using System.Diagnostics;
+
+namespace H.ReactiveUI;
+
+#pragma warning disable CA1822 // Mark members as static
+
+public partial class WebInteractionManager
+{
+    public void Register()
+    {
+        _ = WebInteractions.OpenUrl.RegisterHandler(static context =>
+        {
+            var url = context.Input;
+
+            _ = Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true,
+            });
+
+            context.SetOutput(Unit.Default);
+        });
+    }
+}
+#endif
---
+++ src/libs/H.ReactiveUI.CommonInteractions.Shared/Managers/Web/WebInteractionManager.WinUI.cs
@@ -0,0 +1,28 @@
+﻿#if !HAS_WPF && !HAS_AVALONIA
+using System.Reactive;
+using System.Diagnostics;
+using Windows.System;
+
+namespace H.ReactiveUI;
+
+#pragma warning disable CA1822 // Mark members as static
+
+public partial class WebInteractionManager
+{
+    public void Register()
+    {
+        _ = WebInteractions.OpenUrl.RegisterHandler(async static context =>
+        {
+            var url = context.Input;
+
+            _ = await Launcher.LaunchUriAsync(new Uri(url))
+#if HAS_UNO
+                .ConfigureAwait(true)
+#endif
+                ;
+
+            context.SetOutput(Unit.Default);
+        });
+    }
+}
+#endif
